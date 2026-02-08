8e57c7c68232ac2cdb5ac49ccded6e842ef159e2
Mitza <mitzagrebla@yahoo.com>
2026-02-08 15:09:28 +0200

    Test for ag_news dataset

--- src/ag_news_test/main.py
+++ src/ag_news_test/main.py
@@ -4,18 +4,22 @@ from datasets import load_dataset
 from src.folder_organizer.classifier import AIFileClassifier, ClassesDefinition, AppConfig
 from src.folder_organizer.prompts import DirectPromptStrategy
 
-ds = load_dataset("sh0416/ag_news")
 
-print(ds["test"][0])
 
 def main():
     classes_definition = ClassesDefinition.from_yaml(Path("classes.yaml"))
     app_config = AppConfig.from_yaml(Path("app_config.yaml"))
     classifier = AIFileClassifier(classes_definition=classes_definition, app_config=app_config,
-                                           prompt_strategy=DirectPromptStrategy)
+                                           prompt_strategy=DirectPromptStrategy())
     dataset = load_dataset("sh0416/ag_news", split="test")
     for item in dataset:
-        text = item["text"]
+        text = f"{item["title"]}: {item["description"]}"
         label = item["label"]
         prediction = classifier.classify(text)
         print(f"Text: {text}\nTrue Label: {label}\nPredicted Label: {prediction}\n{'-'*50}")
+
+
+if __name__ == "__main__":
+    ds = load_dataset("sh0416/ag_news", split="test")
+    item = ds[0]
+    print(ds[0])
